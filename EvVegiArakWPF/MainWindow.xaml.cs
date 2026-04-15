fce1476377dd377b3b389e5e86a15bc012c18f9e
vecseizs <vecseizs@kkszki.hu>
2026-04-15 09:17:48 +0200

    Beolvasas kesz

--- EvVegiArakWPF/MainWindow.xaml.cs
+++ EvVegiArakWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,9 +26,45 @@ namespace EvVegiArakWPF
             InitializeComponent();
         }
 
+        public void LoadData()
+        {
+            List<Arak> arak = new List<Arak>();
+            string[] beolvas = File.ReadAllLines("TermekekArai.txt");
+
+            for (int i = 1; i < beolvas.Length; i++)
+            {
+                Arak ar = new Arak(beolvas[i]);
+                arak.Add(ar);
+            }
+
+            adatgrid.ItemsSource = arak;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+    }
+
+    class Arak
+    {
+        public int Kod {  get; set; }
+        public string Megnevezes {  get; set; }
+        public string Szeptember { get; set; }
+        public string Október { get; set; }
+        public string November { get; set; }
+        public string December { get; set; }
+
+        public Arak(string sor)
+        {
+            string[] adatok = sor.Split(';');
+
+            Kod = int.Parse(adatok[0]);
+            Megnevezes = adatok[1];
+            Szeptember = adatok[2];
+            Október = adatok[3];
+            November = adatok[4];
+            December = adatok[5];
         }
     }
 }
